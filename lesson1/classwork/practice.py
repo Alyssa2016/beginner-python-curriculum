d2f40163a5cd0cb188acbc46a53616e54afecbed
Alyssa2016 <alyssahan2016@gmail.com>
2026-09-13 01:57:14 +0000

    practice

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,32 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
-
+num=2
+print(num)
 
 
 # Problem 2
-# Create a variable for your height in meters and print it.
-
-
+# Create a variable for your height in meters and print it.=1.75
+height=1.7
+print(height)
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
-
+fav_animal= "red panda."
+print("My favorite animal is a", fav_animal)
 
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
-
+first_name ="Alyssa"
+last_name ="Han"
+print (first_name + " " + last_name) 
 
 
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+first_name = "Alyssa"
+age = 10
+fav_color = "green"
+print ("My name is", first_name, ". My age is", age, ". My favorite color is", fav_color)
