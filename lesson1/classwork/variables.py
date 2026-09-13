cc77f701e3fc593befc553fa36e82c70f72b0ee3
Alyssa2016 <alyssahan2016@gmail.com>
2026-09-13 01:29:28 +0000

    variables

---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,10 @@
+x = 5 # Integer
+print(x)
+
+number = 3.97 # Float
+print(number)
+
+favorite_food = "Pizza" # String
+print("My favorite food is " + favorite_food + ".")
+
+print (4,21, "dogs", 18)
